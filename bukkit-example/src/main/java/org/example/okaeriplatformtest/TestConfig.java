4f8aa8b089e6010a889cb6eb8a9545303f7291d7
PinkLolicorn <pinklolicorngithub@daffit.pl>
2021-04-03 01:58:36 +0200

    Add more comments to the TestConfig

--- bukkit-example/src/main/java/org/example/okaeriplatformtest/TestConfig.java
+++ bukkit-example/src/main/java/org/example/okaeriplatformtest/TestConfig.java
@@ -16,7 +16,8 @@ import lombok.Setter;
 @Names(strategy = NameStrategy.HYPHEN_CASE, modifier = NameModifier.TO_LOWER_CASE)
 public class TestConfig extends OkaeriConfig {
 
-    @Size(min = 1, max = 64)
-    @Comment("Example config value")
-    private String greeting = "Hi!!!!!!!!1111oneone";
+    @Size(min = 1, max = 64) // validation using okaeri-validator
+    @Variable("APP_GREETING") // use jvm property environment variable if available
+    @Comment("Example config value") // built-in comment support
+    private String greeting = "Hi!!!!!!!!1111oneone"; // default values
 }
