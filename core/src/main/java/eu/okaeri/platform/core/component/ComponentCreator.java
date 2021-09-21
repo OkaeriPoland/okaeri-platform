b2c7ef62917e4e3de6d2ee32b84a483073c3d339
Sandra <sandra@okaeri.cloud>
2021-09-21 08:25:28 +0200

    Simplify ComponentCreator implementations, allow overriding default LocaleProvider

--- core/src/main/java/eu/okaeri/platform/core/component/ComponentCreator.java
+++ core/src/main/java/eu/okaeri/platform/core/component/ComponentCreator.java
@@ -1,19 +1,63 @@
 package eu.okaeri.platform.core.component;
 
 import eu.okaeri.injector.Injector;
+import eu.okaeri.platform.core.component.creator.ComponentCreatorRegistry;
 import eu.okaeri.platform.core.component.manifest.BeanManifest;
+import eu.okaeri.platform.core.component.manifest.BeanSource;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 
 import java.lang.reflect.Method;
+import java.util.*;
+import java.util.stream.Collectors;
 
-public interface ComponentCreator {
+@RequiredArgsConstructor
+public abstract class ComponentCreator {
 
-    Object make(BeanManifest beanManifest, Injector injector);
+    private static final Set<BeanSource> SUPPORTED_BEAN_SOURCES = new HashSet<>(Arrays.asList(BeanSource.METHOD, BeanSource.COMPONENT));
 
-    boolean isComponent(Class<?> type);
+    @Getter(AccessLevel.PROTECTED) @NonNull private final ComponentCreatorRegistry creatorRegistry;
+    @Getter(AccessLevel.PROTECTED) @NonNull private final Map<String, Integer> statisticsMap = new TreeMap<>();
 
-    boolean isComponentMethod(Method method);
+    public void increaseStatistics(String identifier, int count) {
+        this.getStatisticsMap().put(identifier, this.getStatisticsMap().getOrDefault(identifier, 0) + 1);
+    }
 
-    void increaseStatistics(String identifier, int count);
+    public boolean isComponent(@NonNull Class<?> type) {
+        return this.getCreatorRegistry().supports(type);
+    }
 
-    void log(String message);
+    public boolean isComponentMethod(@NonNull Method method) {
+        return this.getCreatorRegistry().supports(method);
+    }
+
+    public Object make(@NonNull BeanManifest manifest, @NonNull Injector injector) {
+
+        // validation
+        if (!SUPPORTED_BEAN_SOURCES.contains(manifest.getSource())) {
+            throw new RuntimeException("Cannot transform from source " + manifest.getSource());
+        }
+
+        // use CreatorRegistry to create components
+        Optional<Object> objectOptional = this.getCreatorRegistry().make(this, manifest);
+
+        // returned value does not seem right
+        if (!objectOptional.isPresent()) {
+            throw new IllegalArgumentException("Found unresolvable component: " + manifest);
+        }
+
+        // extract ready object
+        return objectOptional.get();
+    }
+
+    public String getSummaryText(long took) {
+        String statistics = this.getStatisticsMap().entrySet().stream()
+                .map(entry -> entry.getKey() + ": " + entry.getValue())
+                .collect(Collectors.joining(", "));
+        return "= (" + statistics + ") [blocking: " + took + " ms]";
+    }
+
+    public abstract void log(String message);
 }
