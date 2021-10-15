be1832066bad13a875f1701f8f49d1b5c2f832ac
Sandra <sandra@okaeri.cloud>
2021-10-15 07:44:42 +0200

    Fix BukkitResultHandler not being overridden properly, release 0.4.0-preview34

--- bukkit/src/main/java/eu/okaeri/platform/bukkit/commands/BukkitCommandsResultHandler.java
+++ bukkit/src/main/java/eu/okaeri/platform/bukkit/commands/BukkitCommandsResultHandler.java
@@ -1,6 +1,6 @@
 package eu.okaeri.platform.bukkit.commands;
 
-import eu.okaeri.commands.handler.result.DefaultResultHandler;
+import eu.okaeri.commands.bukkit.handler.BukkitResultHandler;
 import eu.okaeri.commands.service.CommandContext;
 import eu.okaeri.commands.service.InvocationContext;
 import eu.okaeri.i18n.message.Message;
@@ -8,7 +8,7 @@ import eu.okaeri.platform.core.i18n.message.Audience;
 import lombok.NonNull;
 import org.bukkit.command.CommandSender;
 
-public class BukkitCommandsResultHandler extends DefaultResultHandler {
+public class BukkitCommandsResultHandler extends BukkitResultHandler {
 
     @Override
     public boolean handle(Object object, @NonNull CommandContext commandContext, @NonNull InvocationContext invocationContext) {
