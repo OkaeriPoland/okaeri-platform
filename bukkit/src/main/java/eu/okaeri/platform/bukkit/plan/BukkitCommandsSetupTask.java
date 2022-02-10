7bd77f2723118d9d868d310380f7d29fc9bb6000
Sandra <sandra@okaeri.cloud>
2022-02-10 03:08:20 +0100

    Enable commands validation with okaeri-commands 0.8.0, release 0.4.0-preview55

--- bukkit/src/main/java/eu/okaeri/platform/bukkit/plan/BukkitCommandsSetupTask.java
+++ bukkit/src/main/java/eu/okaeri/platform/bukkit/plan/BukkitCommandsSetupTask.java
@@ -4,11 +4,14 @@ import eu.okaeri.commands.Commands;
 import eu.okaeri.commands.brigadier.CommandsBrigadierPaper;
 import eu.okaeri.commands.bukkit.CommandsBukkit;
 import eu.okaeri.commands.injector.CommandsInjector;
+import eu.okaeri.commands.injector.CommandsValidator;
 import eu.okaeri.platform.bukkit.OkaeriBukkitPlugin;
 import eu.okaeri.platform.bukkit.commands.BukkitCommandsResultHandler;
 import eu.okaeri.platform.bukkit.commands.BukkitCommandsTasker;
 import eu.okaeri.platform.core.plan.ExecutionTask;
 import eu.okaeri.tasker.core.Tasker;
+import eu.okaeri.validator.OkaeriValidator;
+import eu.okaeri.validator.policy.NullPolicy;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
@@ -27,6 +30,9 @@ public class BukkitCommandsSetupTask implements ExecutionTask<OkaeriBukkitPlugin
 
         // use injector in commands
         commands.registerExtension(new CommandsInjector(platform.getInjector()));
+
+        // enable validation
+        commands.registerExtension(new CommandsValidator(OkaeriValidator.of(NullPolicy.NULLABLE)));
 
         // use brigadier tab-completions if available
         if (BRIGADIER && this.canUseBrigadierPaper()) {
